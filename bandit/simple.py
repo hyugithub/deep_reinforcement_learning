55489b0ad874b014d72d5305af71366dcfc38c51
HYU <23216665+hyugithub@users.noreply.github.com>
2017-11-24 23:26:22 -0500

    simulation

--- bandit/simple.py
+++ bandit/simple.py
@@ -4,6 +4,10 @@
 # configuration of actions
 import numpy as np
 import config
+import matplotlib.pyplot as plt
+import time
+
+ts = time.time()
 
 class Environment:
     def __init__(self, number_actions):
@@ -76,45 +80,54 @@ class Policy:
         return self.action_frequency[action]
 
 # basic simulation configuration
-number_actions = config.number_actions
-number_try = config.number_try
-prob_exploration = config.prob_exploration
 np.random.seed(config.initial_seed)
+optimal_since = []
 
-#binary reward
-env = Environment(number_actions)
-reward_est = Estimator(number_actions)
-policy = Policy(number_actions, prob_exploration)
-
-#book keeping
-for t in np.arange(number_try):
-    # select an action    
-    action = policy.get_action(reward_est)
-    #print(action)
+for run in range(config.num_simulation):
+    number_actions = config.number_actions
+    number_try = config.number_try
+    prob_exploration = config.prob_exploration    
     
-    # observe reward
-    reward = env.get_reward(action)
-
-    # update estimate    
-    reward_est.update(reward)    
+    #binary reward
+    env = Environment(number_actions)
+    reward_est = Estimator(number_actions)
+    policy = Policy(number_actions, prob_exploration)
     
-final_estimate = reward_est.get()
-
-for a in sorted(range(len(env.reward())), key=lambda x: env.reward()[x], reverse=True):
-    print(a,policy.get_action_frequency(a), env.reward()[a])
-
-#for s in reward_est.history():
-#    print("step=%d action=%d reward estimate=%.3f"%s)
+    #book keeping
+    for t in np.arange(number_try):
+        # select an action    
+        action = policy.get_action(reward_est)
+        #print(action)
+        
+        # observe reward
+        reward = env.get_reward(action)
     
-print("optimal policy = %d"%reward_est.optimal_action())
-print("the policy is optimal since %d"%reward_est.optimal_since())
+        # update estimate    
+        reward_est.update(reward)    
+          
+    #for s in reward_est.history():
+    #    print("step=%d action=%d reward estimate=%.3f"%s)
+    optimal_since.append(reward_est.optimal_since())
+        
+    if 0:
+        final_estimate = reward_est.get()
     
-import matplotlib.pyplot as plt
+        for a in sorted(range(len(env.reward())), key=lambda x: env.reward()[x], reverse=True):
+            print(a,policy.get_action_frequency(a), env.reward()[a])        
+        print("optimal policy = %d"%reward_est.optimal_action())
+        print("the policy is optimal since %d"%reward_est.optimal_since())
+        for a in range(number_actions):
+            x,y = zip(*[(s[0],s[2]) for s in reward_est.history() if s[1] == a])
+            #y = [s[2] for s in reward_est.history() if s[1] == a]
+            plt.plot(x, y, '-', label=str(a))
+        plt.legend(loc='best')
+        plt.show()    
 
-for a in range(number_actions):
-    x,y = zip(*[(s[0],s[2]) for s in reward_est.history() if s[1] == a])
-    #y = [s[2] for s in reward_est.history() if s[1] == a]
-    plt.plot(x, y, '-', label=str(a))
-plt.legend(loc='best')
-plt.show()    
-    
+#print(optimal_since)
+
+print("policy converge mean = %d"%np.mean(optimal_since))
+print("policy converge std = %d"%np.std(optimal_since))
+plt.hist(optimal_since, bins='auto')
+plt.gca().set_xscale("log")
+plt.show()
+print("programming running time = %.3f"%(time.time()-ts))
