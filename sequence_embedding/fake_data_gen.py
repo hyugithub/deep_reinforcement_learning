0d75370d23fa4e9eb44762c9e88b216ff76c02b9
hyugithub <23216665+hyugithub@users.noreply.github.com>
2018-08-26 10:23:23 -0700

    checkin

---
+++ sequence_embedding/fake_data_gen.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 17 17:27:08 2016
+
+@author: YUH015
+"""
+
+#!/usr/bin/python
+import math
+import theano
+import datetime
+import random
+from random import shuffle
+import pandas
+#from nltk.tokenize import word_tokenize
+import csv
+import itertools
+import sys
+import os
+import time
+import numpy as np
+#from utils import *
+from datetime import datetime
+#import pydot
+from gru_theano import GRUTheano
+from gru_theano_batch2 import GRU_theano_batch2
+#from gru_theano_batch import GRU_theano_batch
+from gru_theano_l1 import gru_theano_l1
+import config
+#import cProfile, pstats, StringIO
+
+#theano.config.compute_test_value = 'warn'
+
+VOCABULARY_SIZE = 1024
+
+num_rows = 4096*2
+np.random.seed(12345)
+
+with open("fake.training.txt","w") as outfile:
+    for i in np.arange(num_rows):
+        start = np.random.choice(VOCABULARY_SIZE)
+#        outfile.write(str(start)+" ")
+        outfile.write(str(1)+" ")
+        outfile.write(str(0))
+        outfile.write("\n")
+    outfile.close()
